9ba786d645c4ccd398c15da0e240195d0250a2d2
Darryl Chin <darryl@karmalaunch.com>
2015-09-28 13:25:24 -0700

    Add comments to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 var git = require('gulp-git');
 var gulp = require('gulp');
 
-/**
+/*
  * Release Tasks
  */
 
+// Git Tag
 gulp.task('publish:tag', function(done) {
+  // get the version from package.json
   var pkg = JSON.parse(require('fs').readFileSync('./package.json'));
+
+  // prepend the 'v'
   var v = 'v' + pkg.version;
+
+  // Tag message
   var message = 'Release ' + v;
 
+  // tag the current commit
   git.tag(v, message, function (err) {
     if (err) throw err;
+
+    // push to origin
     git.push('origin', v, function (err) {
       if (err) throw err;
       done();
@@ -19,10 +28,14 @@ gulp.task('publish:tag', function(done) {
   });
 });
 
+// NPM Publish
 gulp.task('publish:npm', function(done) {
+  // get the npm child process
   require('child_process')
+    // spawn another npm process with the command of publish
     .spawn('npm', ['publish'], { stdio: 'inherit' })
     .on('close', done);
 });
 
+// Expose a single command to git tag and npm publish
 gulp.task('release', ['publish:tag', 'publish:npm']);
